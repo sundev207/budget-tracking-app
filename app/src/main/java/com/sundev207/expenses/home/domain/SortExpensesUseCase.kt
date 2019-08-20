d2dd1d59e540000b3082b35aa99f618df640b855
sundev <sundev207@outlook.com>
2019-08-20 21:19:33 +0200

    Adjust expense sorting by adding second parameter for comparison

--- app/src/main/java/com/sundev207/expenses/home/domain/SortExpensesUseCase.kt
+++ app/src/main/java/com/sundev207/expenses/home/domain/SortExpensesUseCase.kt
@@ -1,11 +1,12 @@
 package com.sundev207.expenses.home.domain
 
 import com.sundev207.expenses.data.Expense
-import com.sundev207.expenses.util.extensions.toEpochMillis
 
 class SortExpensesUseCase {
 
-    operator fun invoke(expenses: List<Expense>): List<Expense> {
-        return expenses.sortedByDescending { it.date.toEpochMillis() }
+    private val comparator by lazy {
+        compareByDescending(Expense::date).thenByDescending(Expense::createdAt)
     }
+
+    operator fun invoke(expenses: List<Expense>) = expenses.sortedWith(comparator)
 }
