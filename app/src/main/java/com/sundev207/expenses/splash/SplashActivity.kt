90d132b195e869267560b33cd99fe0caa61eb81f
sundev <sundev207@outlook.com>
2019-11-05 23:47:06 +0100

    Make signing in optional

--- app/src/main/java/com/sundev207/expenses/splash/SplashActivity.kt
+++ app/src/main/java/com/sundev207/expenses/splash/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.sundev207.expenses.Application
 import com.sundev207.expenses.R
 import com.sundev207.expenses.authentication.AuthenticationManager
+import com.sundev207.expenses.data.preference.PreferenceDataSource
 import com.sundev207.expenses.home.presentation.HomeActivity
 import com.sundev207.expenses.onboarding.OnboardingActivity
 
@@ -14,7 +15,7 @@ class SplashActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        if (isUserSignedIn()) {
+        if (isUserOnboarded()) {
             HomeActivity.start(this)
         } else {
             OnboardingActivity.start(this)
@@ -23,8 +24,8 @@ class SplashActivity: AppCompatActivity() {
         finish()
     }
 
-    private fun isUserSignedIn(): Boolean {
-        val authenticationManager = AuthenticationManager.getInstance(application as Application)
-        return authenticationManager.isUserSignedIn()
+    private fun isUserOnboarded(): Boolean {
+        val preferenceDataSource = PreferenceDataSource()
+        return preferenceDataSource.getIsUserOnboarded(applicationContext)
     }
 }
